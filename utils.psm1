7dd8cc7b1b05650089669e3d4d7daeb5db66b588
Ricardo Melo <120052617+RicardoMelo275@users.noreply.github.com>
2023-05-25 11:48:20 -0300

    20230525

--- utils.psm1
+++ utils.psm1
@@ -16,78 +16,82 @@ function Utils_GetSHA256 { # Versoes do posh < 3 não contem a funcao get-fileha
     return $hash.Replace("-", "");
 }
 
-function Utils_EnviarEmail {
+# function Utils_EnviarEmail {
 
-    <#
-    .NOTES
-        Nome            : Funcao: EnviarEmail
-        Autor           : RicardoKeso (ricardokeso@ricardokeso.com)
-        Prerequisitos   : PowerShell V2.0 ou Superior
-        Criacao         : 20210428
+#     <#
+#     .NOTES
+#         Nome            : Funcao: EnviarEmail
+#         Autor           : RicardoKeso (ricardokeso@ricardokeso.com)
+#         Prerequisitos   : PowerShell V2.0 ou Superior
+#         Criacao         : 20210428
 
-    .SYNOPSIS 
+#     .SYNOPSIS 
 
-    .DESCRIPTION
-        Enviar E-Mail 
+#     .DESCRIPTION
+#         Enviar E-Mail 
 
-    .EXAMPLE
-        EnviarEmail -remetente "teste@teste.com" -destinatario "ricardokeso@ricardokeso.com" -titulo "teste" -mensagem "Funciona!!!" -senhaRemetente "senhaAqui" -nomeRemetente "TESTE";
+#     .EXAMPLE
+#         EnviarEmail -remetente "teste@teste.com" -destinatario "ricardokeso@ricardokeso.com" -titulo "teste" -mensagem "Funciona!!!" -senhaRemetente "senhaAqui" -nomeRemetente "TESTE";
 
-    .LINK
-        https://www.ricardokeso.com
+#     .LINK
+#         https://www.ricardokeso.com
         
-    #>
+#     #>
 
-    param (
-        [parameter(Mandatory = $true)][String]$remetente, 
-        [parameter(Mandatory = $true)][String]$destinatario, 
-        [parameter(Mandatory = $true)][String]$titulo, 
-        [parameter(Mandatory = $true)][String]$mensagem, 
-        [parameter(Mandatory = $true)][String]$senhaRemetente,
-        [parameter(Mandatory = $true)][String]$nomeRemetente,
-        [String]$anexo
-    );
+#     param (
+#         [parameter(Mandatory = $true)][String]$remetente, 
+#         [parameter(Mandatory = $true)][String]$destinatario, 
+#         [parameter(Mandatory = $true)][String]$titulo, 
+#         [parameter(Mandatory = $true)][String]$mensagem, 
+#         [parameter(Mandatory = $true)][String]$senhaRemetente,
+#         [parameter(Mandatory = $true)][String]$nomeRemetente,
+#         [String]$anexo
+#     );
 
-    $poshVersion = ($true, $false)[!($PSVersionTable["PSVersion"].Major -eq 2)];
-    $SMTPSrv = "smtp.gmail.com";
-    $SMTPPorta = "587";
-    Write-Output -InputObject "ok";
-    # if (!$poshVersion){
-    #     try {
-    #         $senhaSec = ConvertTo-SecureString -String $senhaRemetente -ErrorAction Stop;
-    #         $credencial = New-Object System.Management.Automation.PSCredential($remetente, $senhaSec);
-    #     } catch {
-    #         Write-Output -InputObject "A senha esta incorreta ou nao esta criptografada.";
-    #         Break;
-    #     }
-    # } else {
-    #     $credencial = New-Object System.Net.NetworkCredential($remetente, $senhaRemetente);
-    # }
+#     $poshVersion = ($true, $false)[!($PSVersionTable["PSVersion"].Major -eq 2)];
+#     $SMTPSrv = "smtp.gmail.com";
+#     $SMTPPorta = "587";
 
-    # $mensagem += "`n`n`nEmail enviado por: ($($env:ComputerName + " \ " + $env:UserName)).";
+#     if (!$poshVersion){
+#         try {
+#             $senhaSec = ConvertTo-SecureString -String $senhaRemetente -ErrorAction Stop;
+#             $credencial = New-Object System.Management.Automation.PSCredential($remetente, $senhaSec);
+#         } catch {
+#             Write-Output -InputObject "A senha esta incorreta ou nao esta criptografada.";
+#             Break;
+#         }
+#     } else {
+#         $credencial = New-Object System.Net.NetworkCredential($remetente, $senhaRemetente);
+#     }
 
-    # $message = New-Object System.Net.Mail.MailMessage;
-    # $message.subject = $titulo;
-    # $message.body = $mensagem;
-    # $message.IsBodyHtml = $false;
-    # $message.to.add($destinatario);
-    # $message.from = $nomeRemetente + " " + $remetente;
-    # if($anexo){$message.attachments.add($anexo)}
-    
-    # try {
-    #     $smtp = New-Object System.Net.Mail.SmtpClient($SMTPSrv, $SMTPPorta);
-    #     $smtp.UseDefaultCredentials = $false;
-    #     $smtp.Credentials = $credencial;
-    #     $smtp.EnableSsl = $true;
-    #     $smtp.send($message);
+#     $mensagem += "`n`n`nEmail enviado por: ($($env:ComputerName + " \ " + $env:UserName)).";
 
-    #     Write-Output -InputObject "Email enviado.";
-    # }
-    # catch {
-    #     # Write-Output "Credenciais incorretas.";
-    #     # $Error[0].Exception.Message | Out-File $([string]$MyInvocation.MyCommand + ".log");
-    #     Write-Output -InputObject ($Error[0].Exception.Message);
-    # }
+#     $message = New-Object System.Net.Mail.MailMessage;
+#     $message.subject = $titulo;
+#     $message.body = $mensagem;
+#     $message.IsBodyHtml = $false;
+#     $message.to.add($destinatario);
+#     $message.from = $nomeRemetente + " " + $remetente;
+#     if($anexo){$message.attachments.add($anexo)}
+
+#     try {
+#         $smtp = New-Object System.Net.Mail.SmtpClient($SMTPSrv, $SMTPPorta);
+#         $smtp.UseDefaultCredentials = $false;
+#         $smtp.Credentials = $credencial;
+#         $smtp.EnableSsl = $true;
+#         $smtp.send($message);
+
+#         Write-Output -InputObject "Email enviado.";
+#     }
+#     catch {
+#         # Write-Output "Credenciais incorretas.";
+#         # $Error[0].Exception.Message | Out-File $([string]$MyInvocation.MyCommand + ".log");
+#         Write-Output -InputObject ($Error[0].Exception.Message);
+#     }
+# }
+
+function Utils_EnviarEmail {
+    Write-Output -InputObject "Utils_EnviarEmail";
 }
 
 function Utils_Compactar_7Zip {
